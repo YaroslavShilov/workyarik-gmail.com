5b38bb74b46c58a329f5280046930de53e18e081
Yaroslav Shilov <workyarik@gmail.com>
2021-08-03 15:13:12 +0300

    add modal-window project in complete_js_course

--- Complete_JS_course/Modal-window/script.js
+++ Complete_JS_course/Modal-window/script.js
@@ -1,105 +1,27 @@
 "use strict";
 
-const getSecretNumber = () => Math.trunc(Math.random() * 20) + 1;
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-/**
- *
- * @param {string} message - get text for showing
- */
-const displayMessage = (message) => {
-  document.querySelector(".message").textContent = message;
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
-/**
- *
- * @param {(string | number)} score get text for showing
- */
-const displayScore = (score) => {
-  document.querySelector(".score").textContent = score;
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
-const state = {
-  secretNumber: getSecretNumber(),
-  score: 20,
-  status: "default",
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
 
-  default: {
-    text: "Start guessing...",
-    body: {
-      backgroundColor: "#222222",
-    },
-    number: {
-      width: "15rem",
-    },
-  },
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-  win: {
-    text: "Current Number!",
-    body: {
-      backgroundColor: "#60b347",
-    },
-    number: {
-      width: "30rem",
-    },
-  },
-
-  lose: {
-    text: "You lost the game!!!",
-    body: {
-      backgroundColor: "tomato",
-    },
-    number: {
-      width: "15rem",
-    },
-  },
-};
-
-/**
- * Change game style and html, using state
- * @param {("default" | "win" | "lose")} status - can be [default | win | lose]
- */
-const finishGame = (status) => {
-  const body = document.querySelector("body");
-  const number = document.querySelector(".number");
-
-  displayMessage(state[status].text);
-  body.style.backgroundColor = state[status].body.backgroundColor;
-  number.style.width = state[status].number.width;
-  number.textContent = status !== "default" ? state.secretNumber : "?";
-  state.status = status;
-};
-
-document.querySelector(".check").addEventListener("click", () => {
-  const guess = Number(document.querySelector(".guess").value);
-
-  // When there's no input
-  if (!guess) displayMessage("No number!");
-  // When player wins
-  else if (guess === state.secretNumber) {
-    const highScore = document.querySelector(".highscore");
-
-    finishGame("win");
-    if (state.score > highScore.textContent)
-      highScore.textContent = state.score;
-  }
-  // When player doesn't win
-  else if (state.status === "default") {
-    --state.score;
-
-    if (state.score <= 0) {
-      finishGame("lose");
-      displayScore(0);
-    } else {
-      displayMessage(guess > state.secretNumber ? "Too high!" : "Too low!");
-      displayScore(state.score);
-    }
-  }
-});
-
-document.querySelector(".again").addEventListener("click", () => {
-  finishGame("default");
-  displayScore((state.score = 20));
-  state.secretNumber = getSecretNumber();
-
-  document.querySelector(".guess").value = "";
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
 });
