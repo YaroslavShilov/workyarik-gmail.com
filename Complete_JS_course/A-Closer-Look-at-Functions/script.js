dd36908bf42353d4c6964793276768e398e42631
Yaroslav Shilov <workyarik@gmail.com>
2021-08-31 09:47:37 +0300

    Finish lesson 136 in complete_js_course

--- Complete_JS_course/A-Closer-Look-at-Functions/script.js
+++ Complete_JS_course/A-Closer-Look-at-Functions/script.js
@@ -314,6 +314,9 @@ function hell2(test) {
 runOnce();
 */
 
+//////////////////////////////////////////////////////////
+// Closure
+/*
 const secureBooking = function () {
   let passengerCount = 0;
 
@@ -332,3 +335,45 @@ booker(); // 1 passengers
 booker(); // 2 passengers
 booker(); // 3 passengers
 console.dir(booker);
+*/
+/////////////////////////////////////////////////////////
+// More Closure Examples
+
+// Example 1
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f(); // 46
+
+// Re-assigning f function
+h();
+f(); // 1554
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait}sec`);
+};
+
+const perGroup = 1000;
+
+boardPassengers(180, 3);
