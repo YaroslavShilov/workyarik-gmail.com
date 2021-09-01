39e4aa94290ce4b4c8bad96e3f68cb01ac8f7df6
Yaroslav Shilov <workyarik@gmail.com>
2021-09-01 22:11:32 +0300

    Finish lesson 139 in complite_js_course

--- Complete_JS_course/A-Closer-Look-at-Functions/script.js
+++ Complete_JS_course/A-Closer-Look-at-Functions/script.js
@@ -340,6 +340,7 @@ console.dir(booker);
 // More Closure Examples
 
 // Example 1
+/*
 let f;
 const g = function () {
   const a = 23;
@@ -377,3 +378,28 @@ const boardPassengers = function (n, wait) {
 const perGroup = 1000;
 
 boardPassengers(180, 3);
+*/
+
+/////////////////////////////////////////////
+// Coding Challenge #2
+
+// This is more of a thinking challenge than a coding challenge
+// Take the IIFE below and at the end of the function,
+// attach an event listener that changes the color of
+// the selected h1 element to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+//
+// and now explain to YOURSELF (or someone around you)
+// WHY this worked! Take all the time you need. Think
+// about WHEN exactly the callback function is executed,
+// and what that means for the variables involved in this example.
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+
+  document.body.addEventListener("click", () =>
+    header.style.color === "blue"
+      ? (header.style.color = "red")
+      : (header.style.color = "blue")
+  );
+})();
