3ede88092314cbd72661c30544795ff17e421f6c
Yaroslav Shilov <workyarik@gmail.com>
2021-09-17 09:28:18 +0300

    Finish lesson 182 in complete_js_course

--- Complete_JS_course/Projects/Bankist-website/script.js
+++ Complete_JS_course/Projects/Bankist-website/script.js
@@ -69,3 +69,73 @@ message.style.backgroundColor = '#37383d';
 message.style.width = '120%';
 console.log(message.style.height); // nothing - because we don't have this style
 console.log(message.style.width); // 120%
+
+console.log(getComputedStyle(message)); // Show all style
+console.log(getComputedStyle(message).backgroundColor); // rgb(55, 56, 61) - not #37383d
+console.log(getComputedStyle(message).height); // 90px - not from css, js calculate height on it own
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
+// use Number.parseFloat - because getComputedStyle return string '90px', '90px' + 40 + 'px' = '90px40px'
+
+console.log(getComputedStyle(message).height); // 130px
+
+// Change variable in css
+// in css= :root {--color-primary: #5ec576;}
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.className); // nav__logo
+console.log(logo.alt); // Bankist logo
+logo.alt = 'Beautiful minimalist logo'; // Set a new meaning
+console.log(logo.alt); // Beautiful minimalist logo
+
+// Get absolute URL, but in HTML we use relative URL 'img/logo.png'
+console.log(logo.src); // http://localhost:63342/100DaysOfCode/Complete_JS_course/Projects/Bankist-website/img/logo.png
+console.log(logo.getAttribute('src')); // img/logo.png - Get relative URL
+
+// Check before get absolute or relative URL
+const navLink = document.querySelector('.nav__link--btn'); // anchor
+// I hide some url after localhost for comfort reading
+console.log(navLink.href); // http://localhost:.../index.html?_ijt=ts2vm3kacursna75hfdbir6pt5&_ij_reload=RELOAD_ON_SAVE#
+console.log(navLink.getAttribute('href')); // #
+
+// Both of them are absolute anyway
+const link = document.querySelector('.twitter-link');
+console.log(link.href); // https://twitter.com/jonasschmedtman
+console.log(link.getAttribute('href')); // https://twitter.com/jonasschmedtman
+
+// Non-standard attributes
+console.log(logo.designer); // undefined - it isn't the standard attribute
+console.log(logo.getAttribute('designer')); // Jonas
+
+// The wrong way to add a new attribute
+logo.newAttr = 'test'; // but it doesn't show in html
+console.log(logo.newAttr); // test
+console.log(logo.getAttribute('newAttr')); // null
+
+// The right way to add a new attribute
+logo.setAttribute('company', 'Bankist');
+console.log(logo.getAttribute('company')); // Bankist
+
+// Data attributes
+// in html <img data-version-number="3.0" src="..." alt="..."/>
+console.log(logo.getAttribute('data-version-number')); // 3.0 - the wrong way
+console.log(logo.dataset.versionNumber); // 3.0 - it's better way
+
+// Classes
+logo.classList.add('c');
+console.log(logo.classList); // DOMTokenList ['nav__logo', 'c', value: 'nav__logo c']
+console.log(logo.classList.value); // nav__logo c
+
+logo.classList.remove('c'); // remove
+console.log(logo.classList.value); // nav__logo
+
+logo.classList.toggle('c'); // toggle add / remove
+console.log(logo.classList.value); // nav__logo c
+
+console.log(logo.classList.contains('c')); // true - check includes or not
+
+// Don't use this
+logo.classList = 'Jonas'; // delete all classes, and set only 'Jonas'
